d984dceb1917e5e05db75f4d701a1d91b714a322
Mark Helt <heltmm@gmail.com>
2017-12-29 15:36:06 -0800

    action cable

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import ActionCable from 'actioncable'
+import Reading from './components/reading'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { readings: [] }
+  }
+
+  componentDidMount() {
+    window.fetch('https://weather-station-.herokuapp.com/').then(data => {
+      data.json().then(res => {
+        this.setState({readings: res})
+      })
+    })
+
+    const cable = ActionCable.createConsumer('ws://weather-station-.herokuapp.com//cable')
+    this.sub = cable.subscriptions.create('ReadingsChannel', {
+      received: this.handleReceiveNewReadings
+    })
+  }
+
+  handleReceiveNewReadings = ({ newReadings }) => {
+    console.log("made it")
+    if (newReadings !== this.state.readings) {
+      this.setState({ readings: newReadings })
+      console.log(this.state.readings)
+    }
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Readings: {this.state.readings.length}</h1>
+          {
+            this.state.readings.map((reading) => (
+              <Reading
+                key={reading.id}
+                temperature={reading.temperature}
+                humidity={reading.humidity}
+              />
+            ))
+          }
       </div>
     );
   }
 }
 
-export default App;
+export default App
---
+++ src/components/reading.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+
+const Reading = (props) => {
+  return <p>Temp: {props.temperature}, Humidity: {props.humidity}</p>;
+}
+export default Reading
