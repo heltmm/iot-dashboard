2a3e7e00be775ea917102a1bc78ba493cedb0625
Mark Helt <heltmm@gmail.com>
2017-12-30 13:25:49 -0800

    add semantic ui for sidebar and navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 import './App.css'
 import ActionCable from 'actioncable'
 import Device from './components/device'
-
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header, Input } from 'semantic-ui-react'
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { devices: [] }
+    this.state = {
+      devices: [],
+      visible: false,
+      activeItem: 'home'}
   }
 
   componentDidMount() {
@@ -23,7 +26,16 @@ class App extends Component {
     // this.sub = cable.subscriptions.create('ReadingsChannel', {
     //   received: this.handleReceiveNewReadings
     // })
+    setInterval( () => {
+      this.setState({
+        curTime : new Date().toLocaleString()
+      })
+    },1000)
   }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   // handleReceiveNewReadings = ({ readings }) => {
   //
@@ -35,18 +47,58 @@ class App extends Component {
 
 
   render() {
+    const { visible } = this.state
+    const { activeItem } = this.state
+
     return (
       <div>
-        <h1>Devices:</h1>
-          {
-            this.state.devices.map((device) => (
-              <Device
-                key={device.id}
-                name={device.name}
-                location={device.location}
-              />
-            ))
-          }
+          <Menu icon inverted>
+            <Menu.Item onClick={this.toggleVisibility}>
+              <Icon name='content' />
+            </Menu.Item>
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+          <Menu.Item>
+            Date: {this.state.curTime}
+          </Menu.Item>
+          <Menu.Menu position='right'>
+            <Menu.Item>
+              <Input icon='search' placeholder='Search...' />
+            </Menu.Item>
+            <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick} />
+          </Menu.Menu>
+        </Menu>
+
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
+            <Menu.Item name='home'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item name='gamepad'>
+              <Icon name='gamepad' />
+              Devices
+            </Menu.Item>
+            <Menu.Item name='camera'>
+              <Icon name='camera' />
+              New Device
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Header as='h3'>Application Content</Header>
+                <h1>Devices:</h1>
+                  {
+                    this.state.devices.map((device) => (
+                      <Device
+                        key={device.id}
+                        name={device.name}
+                        location={device.location}
+                      />
+                    ))
+                  }
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </div>
     );
   }
